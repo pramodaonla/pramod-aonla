37a18aadf11c90f68e14799f2e79049aa5049dfa
pramodaonla <pramodverma395@gmail.com>
2025-12-14 16:31:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json()); // JSON body read karne ke liye
+
 app.get("/", (req, res) => {
-  res.send("Backend working 🚀");
+  res.send("Backend working");
 });
 
-const PORT = process.env.PORT || 5000;
+// 👇 MESSAGE TEST API
+app.post("/message", (req, res) => {
+  const { message } = req.body;
+  res.json({
+    success: true,
+    received: message
+  });
+});
+
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log("Server started on", PORT);
 });
