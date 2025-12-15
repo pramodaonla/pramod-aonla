470f0f0f6cb9c1a07b48e96ba086d59d6215086e
pramodaonla <pramodverma395@gmail.com>
2025-12-15 09:29:17 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,12 +1,21 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-
-  resetOTP: String,
-  resetOTPExpire: Date,
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
