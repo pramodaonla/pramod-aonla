2327cc7b5a7ca3e3f8edc95511830f99614380ce
pramodaonla <pramodverma395@gmail.com>
2025-12-16 07:59:15 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
+  email: { type: String, unique: true },
   password: String,
-  resetPasswordToken: String,
-  resetPasswordExpire: Date,
+  verified: { type: Boolean, default: false },
+  otp: String,
+  otpExpiry: Date
 });
 
 module.exports = mongoose.model("User", userSchema);
