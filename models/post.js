9818ab16cdc5acde51641e858329234d4c084647
pramodaonla <pramodverma395@gmail.com>
2025-12-17 09:05:33 +0530

    Update post.js

--- models/post.js
+++ models/post.js
@@ -1,1 +1,24 @@
+const mongoose = require("mongoose");
 
+const PostSchema = new mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  kind: {
+    type: String,
+    enum: ["photo", "video", "story"],
+    default: "photo"
+  },
+  media: {
+    type: String,   // base64 (अभी)
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Post", PostSchema);
