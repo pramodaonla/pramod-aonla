976456e3e7b46ab1be716c148fc1de9e71ca41be
pramodaonla <pramodverma395@gmail.com>
2025-12-18 07:02:57 +0530

    Update sendMail.js

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,1 +1,22 @@
+const nodemailer = require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
+  }
+});
+
+async function sendMail(to, subject, html) {
+  await transporter.sendMail({
+    from: `"Pramod Aonla" <${process.env.MAIL_USER}>`,
+    to,
+    subject,
+    html
+  });
+}
+
+module.exports = sendMail;
