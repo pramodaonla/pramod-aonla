9ef4959cba580c089db6e9e7a8fcf5947c801fb9
pramodaonla <pramodverma395@gmail.com>
2025-12-19 16:27:22 +0530

    Update mail..js

--- config/mail..js
+++ config/mail..js
@@ -1,8 +1,8 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
+  host: "smtp.gmail.com",
+  port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
