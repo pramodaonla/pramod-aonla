241f374f483a954f113966a198fbf31e65d0fb6f
pramodaonla <pramodverma395@gmail.com>
2025-12-19 16:50:37 +0530

    Update mail..js

--- config/mail..js
+++ config/mail..js
@@ -6,8 +6,8 @@ const transporter = nodemailer.createTransport({
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 export default transporter;
