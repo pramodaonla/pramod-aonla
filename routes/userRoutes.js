cc2ea7ca2944f84632aa65e3775cc6e91d6c3eb2
pramodaonla <pramodverma395@gmail.com>
2025-12-19 16:47:24 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 import User from "../models/User.js";
-import sendMail from "../config/mail.js";
+import transporter from "../config/mail.js";
 
 const router = express.Router();
 
