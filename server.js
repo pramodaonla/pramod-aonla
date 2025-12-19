bcf45d0304161e647e4a1c08f2814edb3c4b1e58
pramodaonla <pramodverma395@gmail.com>
2025-12-19 13:33:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend Running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server started on " + PORT);
+});
