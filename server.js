39def6f68d6352e667c92e43c9d52d228995ab83
pramodaonla <pramodverma395@gmail.com>
2025-12-19 18:00:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("Server started on " + PORT);
+app.listen(3000, () => {
+  console.log("Server running on " + PORT);
 });
