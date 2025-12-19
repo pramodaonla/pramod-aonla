00d0cfec685e987fd63df3653b2b2dae5615d5ef
pramodaonla <pramodverma395@gmail.com>
2025-12-20 04:36:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -10,7 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", userRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
