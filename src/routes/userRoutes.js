2d57935996afe805bd3e4177147cb21949012bd5
pramodaonla <pramodverma395@gmail.com>
2025-12-20 04:40:19 +0530

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,1 +1,8 @@
+import express from "express";
+const router = express.Router();
 
+router.get("/test", (req, res) => {
+  res.json({ message: "User routes working" });
+});
+
+export default router;
