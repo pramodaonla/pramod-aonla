fca5c0ee09cfd8b4a2c1713391abe2050e974be5
pramodaonla <pramodverma395@gmail.com>
2025-12-20 09:24:20 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,3 +1,8 @@
+import express from "express";
 import { verifyOtp } from "../controllers/authController.js";
 
+const router = express.Router();
+
 router.post("/verify-otp", verifyOtp);
+
+export default router;
