93cde7871239ea155f19c328b587566fe22978a6
pramodaonla <pramodverma395@gmail.com>
2025-12-21 14:45:37 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,14 @@
+import express from "express";
+import authMiddleware from "../middleware/authMiddleware.js";
 
+const router = express.Router();
+
+// 🔒 Protected route
+router.get("/profile", authMiddleware, (req, res) => {
+  res.json({
+    message: "Profile fetched successfully",
+    user: req.user
+  });
+});
+
+export default router;
