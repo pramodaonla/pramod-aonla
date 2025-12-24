62b1c9a9f9274e218b9d1c3aee23f6b79c5fc9a9
pramodaonla <pramodverma395@gmail.com>
2025-12-24 22:03:23 +0530

    Update postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,6 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
-import {
-  createPost,
-  getMyPosts
-} from "../controllers/postController.js";
+import { createPost, getMyPosts } from "../controllers/postController.js";
 
 const router = express.Router();
 
