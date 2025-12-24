bd0156ef5273f651fd92aebf567076d3983d65b8
pramodaonla <pramodverma395@gmail.com>
2025-12-24 15:34:05 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,24 +1,25 @@
-import express from "express";
-import {
+const express = require("express");
+const {
   register,
   verifyOtp,
   login,
   forgotPassword,
   resetPassword
-} from "../controllers/authController.js";
-
-import authMiddleware from "../middleware/authMiddleware.js";
+} = require("../controllers/authController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
+/* PUBLIC ROUTES */
 router.post("/register", register);
 router.post("/verify-otp", verifyOtp);
 router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
 
+/* PROTECTED ROUTE */
 router.get("/me", authMiddleware, (req, res) => {
-  res.json({ user: req.user });
+  res.json({ message: "Authenticated user", user: req.user });
 });
 
-export default router;
+module.exports = router;
