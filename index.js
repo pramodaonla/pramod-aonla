612509979498ecec4a9cb6b416b756c6bc03c2a3
pramodaonla <pramodverma395@gmail.com>
2025-12-24 23:17:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import express from "express";
-import authRoutes from "./routes/authRoutes.js";
-import postRoutes from "./routes/postRoutes.js";
+import dotenv from "dotenv";
+
 import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
