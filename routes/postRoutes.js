6fa0bd97eb184718476bd5615743b13b571fb3c1
pramodaonla <pramodverma395@gmail.com>
2025-12-24 20:41:53 +0530

    Update postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const authMiddleware = require("../middleware/authMiddleware");
-const { createPost, getMyPosts } = require("../controllers/postController");
+import express from "express";
+import authMiddleware from "../middleware/authMiddleware.js";
+import { createPost, getMyPosts } from "../controllers/postController.js";
 
 const router = express.Router();
 
 router.post("/create", authMiddleware, createPost);
 router.get("/my", authMiddleware, getMyPosts);
 
-module.exports = router;
+export default router;
