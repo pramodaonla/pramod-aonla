cd0a5d081fd6cf832512885295cc3ca6a71555fe
pramodaonla <pramodverma395@gmail.com>
2025-12-24 22:02:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
 
+import express from "express";
 import authRoutes from "./routes/authRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-
-dotenv.config();
 
 const app = express();
 app.use(express.json());
