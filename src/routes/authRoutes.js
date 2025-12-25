406aa318d4473b969c6d6b1255f21604357bc3c2
pramodaonla <pramodverma395@gmail.com>
2025-12-25 20:44:34 +0530

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,1 +1,14 @@
+import express from "express";
+import { loginUser, registerUser } from "../../controllers/authController.js";
 
+const router = express.Router();
+
+/* TEST */
+router.get("/test", (req, res) => {
+  res.json({ message: "Auth route working ✅" });
+});
+
+router.post("/login", loginUser);
+router.post("/register", registerUser);
+
+export default router;
