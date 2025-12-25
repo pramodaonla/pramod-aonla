cba95cebec68607a36ab016a9b888593f033b02d
pramodaonla <pramodverma395@gmail.com>
2025-12-25 10:03:13 +0530

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,4 +5,8 @@ router.get("/test", (req, res) => {
   res.json({ message: "User routes working ✅" });
 });
 
+router.get("/profile", (req, res) => {
+  res.json({ user: "Pramod Kumar" });
+});
+
 export default router;
