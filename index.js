dc7f418f8f38fc7459dc15eb689f2fa43f44c8f3
pramodaonla <pramodverma395@gmail.com>
2025-12-25 09:42:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,27 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
-import userRoutes from "./routes/userRoutes.js";
-import postRoutes from "./routes/postRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./src/routes/userRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
+// test route
 app.get("/", (req, res) => {
-  res.send("API running successfully 🚀");
+  res.send("Server running ✅");
 });
 
+// routes
 app.use("/api/users", userRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/auth", authRoutes);
 
+// PORT (Render + local dono ke liye)
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
