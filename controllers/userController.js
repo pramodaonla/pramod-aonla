b07c676b6285f01c9a4007fd6b58a9ca5e02466f
pramodaonla <pramodverma395@gmail.com>
2025-12-25 20:13:40 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,63 +1,19 @@
 import User from "../models/User.js";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 
-/* Generate JWT */
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
-};
-
-/* Register user */
-export const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
-
+/* GET PROFILE */
+export const getProfile = async (req, res) => {
   try {
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-      return res.status(400).json({ message: "User already exists" });
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await User.create({ name, email, password: hashedPassword });
-    res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(user._id),
+    res.json({
+      success: true,
+      user,
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
-  }
-};
-
-/* Login user */
-export const loginUser = async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-    if (user && (await bcrypt.compare(password, user.password))) {
-      res.json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        token: generateToken(user._id),
-      });
-    } else {
-      res.status(401).json({ message: "Invalid credentials" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-/* Get profile */
-export const getProfile = async (req, res) => {
-  const user = await User.findById(req.user.id).select("-password");
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404).json({ message: "User not found" });
   }
 };
