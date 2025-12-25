7174a2397276b12a25a844732241cc6a3fc74df2
pramodaonla <pramodverma395@gmail.com>
2025-12-25 10:08:42 +0530

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,17 @@
 import express from "express";
 const router = express.Router();
 
+/* ✅ TEST */
 router.get("/test", (req, res) => {
   res.json({ message: "User routes working ✅" });
 });
 
+/* ✅ PROFILE */
 router.get("/profile", (req, res) => {
-  res.json({ user: "Pramod Kumar" });
+  res.json({
+    name: "Pramod Kumar",
+    status: "Profile working ✅",
+  });
 });
 
 export default router;
