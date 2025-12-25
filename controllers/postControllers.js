628cf6ae2f505f6a966a2b746f29d97149d36bd4
pramodaonla <pramodverma395@gmail.com>
2025-12-25 08:48:41 +0530

    Update postControllers.js

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,11 +1,11 @@
-export const createPost = async (req, res) => {
+export const createPost = (req, res) => {
   res.json({
     message: "Post created successfully",
-    user: req.user
+    user: req.user || null
   });
 };
 
-export const getMyPosts = async (req, res) => {
+export const getMyPosts = (req, res) => {
   res.json({
     message: "My posts fetched",
     posts: []
