abc895219176a99d8e1b3f52688d2617b43944f1
pramodaonla <pramodverma395@gmail.com>
2025-12-25 22:40:18 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,13 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000,
-    });
-
-    console.log("✅ MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB connected");
   } catch (error) {
-    console.error("❌ MongoDB connection failed:", error.message);
+    console.error("❌ MongoDB error:", error.message);
     process.exit(1);
   }
 };
