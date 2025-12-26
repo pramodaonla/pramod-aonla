4edb527508d635eaca4478a4fd466ebeabc57a98
pramodaonla <pramodverma395@gmail.com>
2025-12-26 19:16:59 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true, lowercase: true },
+    email: { type: String, required: true, lowercase: true, unique: true },
     password: { type: String, required: true },
     isVerified: { type: Boolean, default: false }
   },
