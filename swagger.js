f72adb0fabacdfbca05a409daa35db484414facc
pramodaonla <pramodverma395@gmail.com>
2025-12-26 22:05:08 +0530

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -1,39 +1,14 @@
-import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
-
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "BiggEyes API",
-      version: "1.0.0",
-      description: "Authentication APIs with Email OTP",
-    },
-    servers: [
-      {
-        url: "http://localhost:3000",
-      },
-      {
-        url: "https://YOUR-RENDER-URL.onrender.com",
-      },
-    ],
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: "http",
-          scheme: "bearer",
-          bearerFormat: "JWT",
-        },
-      },
-    },
-    security: [{ bearerAuth: [] }],
-  },
-  apis: ["./src/routes/*.js"], // 👈 important
-};
-
-const swaggerSpec = swaggerJSDoc(options);
+import YAML from "yamljs";
 
 export const swaggerDocs = (app) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  console.log("📘 Swagger running at /api-docs");
+  const swaggerDocument = YAML.load("./swagger.yaml");
+
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+  );
+
+  console.log("📄 Swagger Docs available at /api-docs");
 };
