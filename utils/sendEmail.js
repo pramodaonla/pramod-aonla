cf6e51239f1e0e6f529fc1ff072ef3840492818d
pramodaonla <pramodverma395@gmail.com>
2025-12-26 19:20:39 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,15 +3,10 @@ import sgMail from "@sendgrid/mail";
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export const sendEmail = async (to, subject, html) => {
-  try {
-    await sgMail.send({
-      to,
-      from: { email: process.env.MAIL_FROM, name: "BiggEyes" },
-      subject,
-      html
-    });
-    console.log("✅ Email sent:", to);
-  } catch (err) {
-    console.error("❌ SendGrid Error:", err?.response?.body || err.message);
-  }
+  await sgMail.send({
+    to,
+    from: { email: process.env.MAIL_FROM, name: "BiggEyes" },
+    subject,
+    html
+  });
 };
