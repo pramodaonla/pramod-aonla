94e3b10605e5dc2667ffd266f2b90105bd34c128
pramodaonla <pramodverma395@gmail.com>
2025-12-26 14:06:01 +0530

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { getProfile } from "../../../controllers/userController.js";
-import { protect } from "../../../middleware/authMiddleware.js";
+import { getProfile } from "../../controllers/userController.js";
+import { protect } from "../../middleware/authMiddleware.js";
 
 const router = express.Router();
 
