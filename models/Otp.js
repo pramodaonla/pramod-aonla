9582f0d2815289cd8e24f307d48cd016660fadbb
pramodaonla <pramodverma395@gmail.com>
2025-12-26 08:18:52 +0530

    Update Otp.js

--- models/Otp.js
+++ models/Otp.js
@@ -1,19 +1,9 @@
 import mongoose from "mongoose";
 
 const otpSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    lowercase: true
-  },
-  otp: {
-    type: String,
-    required: true
-  },
-  expiresAt: {
-    type: Date,
-    required: true
-  }
+  email: { type: String, required: true, lowercase: true },
+  otp: { type: String, required: true },
+  expiresAt: { type: Date, required: true }
 }, { timestamps: true });
 
 export default mongoose.model("Otp", otpSchema);
