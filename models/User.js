3f38272a1f1002c4c98e02150e5f320a6bd2ebe8
pramodaonla <pramodverma395@gmail.com>
2025-12-26 13:26:34 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true },
-    isVerified: { type: Boolean, default: false } // ✅ added
+    isVerified: { type: Boolean, default: false }
   },
   { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
-export default User;
+export default mongoose.model("User", userSchema);
