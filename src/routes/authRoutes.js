24d70f03e0573a98ae240eb1e527955f076cc863
pramodaonla <pramodverma395@gmail.com>
2025-12-26 22:20:22 +0530

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,51 +2,13 @@ import express from "express";
 import {
   registerUser,
   loginUser,
-  verifyOtp,
-  forgotPassword,
-  resetPassword
+  verifyOtp
 } from "../controllers/authController.js";
 
 const router = express.Router();
 
-/**
- * @swagger
- * /api/auth/register:
- *   post:
- *     summary: Register new user (OTP sent on email)
- */
 router.post("/register", registerUser);
-
-/**
- * @swagger
- * /api/auth/verify-otp:
- *   post:
- *     summary: Verify OTP for account activation
- */
 router.post("/verify-otp", verifyOtp);
-
-/**
- * @swagger
- * /api/auth/login:
- *   post:
- *     summary: Login user
- */
 router.post("/login", loginUser);
-
-/**
- * @swagger
- * /api/auth/forgot-password:
- *   post:
- *     summary: Send OTP for password reset
- */
-router.post("/forgot-password", forgotPassword);
-
-/**
- * @swagger
- * /api/auth/reset-password:
- *   post:
- *     summary: Reset password using OTP
- */
-router.post("/reset-password", resetPassword);
 
 export default router;
