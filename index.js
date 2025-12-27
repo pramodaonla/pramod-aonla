09179fe54d5be59803f9b2005c0081e6244022ba
pramodaonla <pramodverma395@gmail.com>
2025-12-27 08:38:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -14,6 +16,11 @@ connectDB();
 
 app.use("/api/auth", authRoutes);
 
-app.listen(process.env.PORT || 3000, () =>
-  console.log("🚀 Server running")
-);
+app.get("/", (req, res) => {
+  res.send("API running ✅");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on ${PORT}`);
+});
