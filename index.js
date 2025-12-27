2431d9ee2728f2379e995adebeff6639c61d3c48
pramodaonla <pramodverma395@gmail.com>
2025-12-27 07:00:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoutes.js";
+import authRoutes from "./src/routes/authRoutes.js";
 
 dotenv.config();
 
