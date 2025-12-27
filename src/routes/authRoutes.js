e5dd8b0f9513b22ec49a67436368bba54609b2b0
pramodaonla <pramodverma395@gmail.com>
2025-12-27 07:08:58 +0530

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,9 @@ import express from "express";
 import {
   registerUser,
   loginUser,
-  verifyOtp
+  verifyOtp,
+  forgotPassword,
+  resetPassword
 } from "../../controllers/authController.js";
 
 const router = express.Router();
@@ -10,5 +12,7 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/verify-otp", verifyOtp);
 router.post("/login", loginUser);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 
 export default router;
