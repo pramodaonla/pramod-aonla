26ae60ceaa4055de034f5b7db419844ac45a3a97
pramodaonla <pramodverma395@gmail.com>
2025-12-27 07:34:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
 import connectDB from "./config/db.js";
-import authRoutes from "./src/routes/authRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-// DB
 connectDB();
 
-// Routes
 app.use("/api/auth", authRoutes);
 
-// Health
-app.get("/", (req, res) => {
-  res.send("API is running ✅");
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+app.listen(process.env.PORT || 3000, () =>
+  console.log("🚀 Server running")
+);
